fd818da82985c144c4f30468a1d655aa96aca8cc
KiseraTimon <timonmarick@gmail.com>
2025-12-09 18:53:17 +0300

    #7 - Registering Blueprints

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = create_app()
 
 # Entry Point
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -6,4 +6,10 @@ def create_app() -> Flask:
     # Flask Object
     app = Flask(__name__)
 
+    # Importing Blueprints
+    from website.modules.routes import routes
+
+    # Registering Blueprints
+    app.register_blueprint(routes, url_prefix="/")
+
     return app
