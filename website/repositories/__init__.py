c92d975ec3946bdb00ec24c03813f0c587f2e329
KiseraTimon <timonmarick@gmail.com>
2026-01-08 11:32:55 +0300

    #121 - Updated Repository Package File

--- website/repositories/__init__.py
+++ website/repositories/__init__.py
@@ -1,8 +1,12 @@
 
 from .base_repository import BaseRepository
 from .order_repository import OrderRepository
+from .user_repository import UserRepository
+from .customer_repository import CustomerRepository
 
 __all__ = [
     "BaseRepository",
-    "OrderRepository"
+    "OrderRepository",
+    "UserRepository",
+    "CustomerRepository"
 ]
