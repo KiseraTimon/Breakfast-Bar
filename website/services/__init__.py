3cda38bd10da31c4073d43dff4c82eb9fc464979
KiseraTimon <timonmarick@gmail.com>
2026-01-08 11:50:39 +0300

    #126 - Code Cleanup

--- website/services/__init__.py
+++ website/services/__init__.py
@@ -1,9 +1,5 @@
-from .auth_validator import AuthValidator
-from .validation_result import ValidationResult
 from .auth_service import AuthService
 
 __all__ = [
-    "ValidationResult",
-    "AuthValidator",
     "AuthService"
 ]
