90c6ff680bb2f68beebbe824686a86573b1c26d0
KiseraTimon <timonmarick@gmail.com>
2026-01-10 21:53:15 +0300

    #149 - Services Package File Update

--- website/services/__init__.py
+++ website/services/__init__.py
@@ -1,7 +1,9 @@
 from .auth_service import AuthService
 from .dashboard_service import DashboardService
+from .menu_service import MenuService
 
 __all__ = [
     "AuthService",
-    "DashboardService"
+    "DashboardService",
+    "MenuService"
 ]
